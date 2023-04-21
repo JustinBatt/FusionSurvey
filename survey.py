6531b7e3bcf6c8d5670305211231935fb977ac6d
JustinBatt <53529152+JustinBatt@users.noreply.github.com>
2023-04-21 16:55:33 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import xml.etree.ElementTree as ET
+from github import Github
 
 # Define the survey questions as a dictionary
 survey_questions = {
@@ -8,13 +9,19 @@ survey_questions = {
     "feedback": "Do you have any feedback for us?"
 }
 
-# Define a function to save the responses to an XML document
+# Define a function to save the responses to an XML document in a GitHub path
 def save_responses(responses):
+    # Initialize the GitHub repository
+    g = Github("YOUR_GITHUB_ACCESS_TOKEN")
+    repo = g.get_repo("YOUR_GITHUB_USERNAME/FusionSurvey")
+    # Create the XML tree
     root = ET.Element("survey_responses")
     for key, value in responses.items():
         ET.SubElement(root, key).text = value
     tree = ET.ElementTree(root)
-    tree.write("/Responses.xml")
+    # Write the XML file to the GitHub path
+    contents = ET.tostring(root, encoding="unicode", method="xml")
+    repo.create_file("survey_responses.xml", "Committing survey responses", contents)
 
 # Create a Streamlit app that displays the survey questions and saves responses
 def main():
