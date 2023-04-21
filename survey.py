47d50cccf436a5841ceac0421b479b8e0c984f96
JustinBatt <53529152+JustinBatt@users.noreply.github.com>
2023-04-21 16:41:28 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import tkinter as tk
 
 # Function to create and update the XML file
 def update_xml(filename, tag, value):
@@ -20,14 +21,40 @@ def update_xml(filename, tag, value):
     tree = ET.ElementTree(root)
     tree.write(filename)
 
-# Ask the user for their response to the first question
-response1 = input("Hi, how are you doing? ")
+# Create the GUI
+root = tk.Tk()
+root.title("Survey")
 
-# Store the response in the XML file
-update_xml("survey.xml", "response1", response1)
+# Function to handle the submission of the form
+def submit_form():
+    # Get the values from the form
+    response1 = entry1.get()
+    response2 = entry2.get()
 
-# Ask the user for their response to the second question
-response2 = input("What's new? ")
+    # Store the responses in the XML file
+    update_xml("survey.xml", "response1", response1)
+    update_xml("survey.xml", "response2", response2)
 
-# Store the response in the XML file
-update_xml("survey.xml", "response2", response2)
+    # Clear the form
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
+
+    # Show a message indicating that the form was submitted
+    tk.messagebox.showinfo("Survey", "Thank you for your response!")
+
+# Add the form elements
+label1 = tk.Label(root, text="Hi, how are you doing?")
+label1.pack()
+entry1 = tk.Entry(root)
+entry1.pack()
+
+label2 = tk.Label(root, text="What's new?")
+label2.pack()
+entry2 = tk.Entry(root)
+entry2.pack()
+
+button = tk.Button(root, text="Submit", command=submit_form)
+button.pack()
+
+# Run the GUI
+root.mainloop()
